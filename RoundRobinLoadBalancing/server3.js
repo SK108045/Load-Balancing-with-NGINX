539e7e590f5798f344f9cff09bbf16860ef42be1
sk10 <124081032+SK108045@users.noreply.github.com>
2024-10-12 10:50:14 +0300

    Update server3.js

--- RoundRobinLoadBalancing/server3.js
+++ RoundRobinLoadBalancing/server3.js
@@ -1,1 +1,12 @@
 
+const http = require('http');
+const port = 3002;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello from Node.js server 1!');
+});
+
+server.listen(port, () => {
+  console.log(`Server 1 running on http://localhost:${port}`);
+});
