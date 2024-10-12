a16f4100df091e2f2b8758890ad06147b51397c6
sk10 <124081032+SK108045@users.noreply.github.com>
2024-10-12 10:49:42 +0300

    Create server2.js

---
+++ RoundRobinLoadBalancing/server2.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const port = 3001;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello from Node.js server 1!');
+});
+
+server.listen(port, () => {
+  console.log(`Server 1 running on http://localhost:${port}`);
+});
